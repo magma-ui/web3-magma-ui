a2b26b3e8e80cb88d3483fcd2692b8b491cfb83a
asdfghjA1 <aaryan14decembersingh@gmail.com>
2024-09-30 18:46:01 +0530

    Fix merge conflicts

--- app/user-components/page.tsx
+++ app/user-components/page.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 export default function UserComponentsPage() {
   return (
-<<<<<<< HEAD
 <>
-This is a test</>
-=======
+This is a test
     <div>
       Hello World
     </div>
->>>>>>> upstream/main
+    </>
   )
 }
