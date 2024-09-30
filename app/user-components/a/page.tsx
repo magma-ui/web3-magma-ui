b6d5dfadc51c34d5da8590c54ae8b464dae5f93a
asdfghjA1 <aaryan14decembersingh@gmail.com>
2024-09-30 18:15:31 +0530

    Added the Components bar setup / Aaryan

---
+++ app/user-components/a/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export default function a() {
+    return(
+        <>
+        <p>This A</p>
+        </>
+    )
+}
--- app/user-components/page.tsx
+++ app/user-components/page.tsx
@@ -1,19 +1,7 @@
-import { title } from "@/components/primitives";
 import React from "react";
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable"
-
-
-
-export default function userComponentsPage() {
+export default function UserComponentsPage() {
   return (
-    <ResizablePanelGroup direction="horizontal">
-      <ResizablePanel maxSize={25} className="p-40 bg-white text-black">One</ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel className="p-40 bg-black text-white">Two</ResizablePanel>
-    </ResizablePanelGroup>
+<>
+This is a test</>
   )
 }
